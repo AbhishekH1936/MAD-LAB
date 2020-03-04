e619744717ade5bdccfbe877a4e22f3508fc03fd
AbhishekH1936 <53899135+AbhishekH1936@users.noreply.github.com>
2020-03-04 15:21:38 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,54 +1,82 @@
-package com.example.ias;
+package com.example.calculator;
+
 import android.os.Bundle;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.content.Intent;
+import android.text.TextUtils;
 import android.view.View;
-import android.widget.ArrayAdapter;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Spinner;
 import android.widget.TextView;
-
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
-
-    EditText e1,e2;
-    Button bt;
-    Spinner s;
-
-    String[] dept_array={"CSE","ECE","IT","ME","CV","ISE"};
-
-    String name,reg,dept;
-
+public class MainActivity extends AppCompatActivity implements OnClickListener
+{
+    //Defining the Views
+    EditText Num1;
+    EditText Num2;
+    Button Add;
+    Button Sub;
+    Button Mul;
+    Button Div;
+    TextView Result;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        e1=(EditText)findViewById(R.id.editText3);
-        e2=(EditText)findViewById(R.id.editText4);
+//Referring the Views
 
-        bt=(Button)findViewById(R.id.button);
-        s=(Spinner)findViewById(R.id.spinner);
-        ArrayAdapter adapter=new ArrayAdapter(MainActivity.this,android.R.layout.simple_spinner_item,dept_array);
-        s.setAdapter(adapter);
+        Num1 = (EditText) findViewById(R.id.editText1);
+        Num2 = (EditText) findViewById(R.id.editText2);
+        Add = (Button) findViewById(R.id.Add);
+        Sub = (Button) findViewById(R.id.Sub);
+        Mul = (Button) findViewById(R.id.Mul);
+        Div = (Button) findViewById(R.id.Div);
+        Result = (TextView) findViewById(R.id.textView);
+// set a listener
+        Add.setOnClickListener(this);
+        Sub.setOnClickListener(this);
+        Mul.setOnClickListener(this);
+        Div.setOnClickListener(this);
+    }
+    @Override
+    public void onClick (View v)
+    {
+        float num1 = 0;
+        float num2 = 0;
+        float result = 0;
+        String oper = " ";
+// check if the fields are empty
+        if(TextUtils.isEmpty(Num1.getText().toString()) ||
+                TextUtils.isEmpty(Num2.getText().toString()))
+            return;
+// read EditText and fill variables with numbers
+        num1 = Float.parseFloat(Num1.getText().toString());
+        num2 = Float.parseFloat(Num2.getText().toString());
+// defines the button that has been clicked and performs the corresponding operation
+// write operation into oper, we will use it later for output
+        switch(v.getId())
+        {
+            case R.id.Add:
+            oper = "+";
+            result = num1 + num2;
+            break;
+            case R.id.Sub:
+            oper = "-";
+            result = num1 - num2;
+            break;
+            case R.id.Mul:
 
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                name=e1.getText().toString();
-                reg=e2.getText().toString();
-                dept=s.getSelectedItem().toString();
-
-                Intent i=new Intent(MainActivity.this, SecondAct.class);
-                i.putExtra("name_key",name);
-                i.putExtra("reg_key",reg);
-                i.putExtra("dept_key",dept);
-
-                startActivity(i);
-            }
-        });
+            oper = "*";
+            result = num1 * num2;
+            break;
+            case R.id.Div:
+            oper = "/";
+            result = num1 / num2;
+            break;
+            default:
+                break;
+        }
+// form the output line
+        Result.setText(num1 + " " + oper + " " + num2 + " = "+ result);
     }
 }
